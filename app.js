91d81f2be0ea344583427cb07b44d29fe208e548
Mateus Ribeiro <mateus@even3.com>
2024-02-20 16:33:53 -0300

    Comentarios

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 function sortear(){
+    // Capturar a qtd de números sorteados bem como de e ate.
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-
+    // Gerar condicional para informar situações onde não é possível sortear.
     if (de >= ate) {
     alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
     return;
@@ -12,7 +13,7 @@ function sortear(){
         alert('A quantidade de números sorteados é maior que a diferença entre os valores, tente novamente.');
         return;
     }    
-
+    // Fazer uma lista com os números sorteados gerados aleatoriamente.
     let sorteados = [];
     let numero;
     
@@ -24,6 +25,8 @@ function sortear(){
         sorteados.push(numero);
     }
 
+    // Exibir no html quais foram os números sorteados.
+
     let resultado = document.getElementById('resultado')
     resultado.innerHTML =  `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
 
@@ -31,9 +34,13 @@ function sortear(){
 
 }
 
+// Sortear número aleatório
+
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// Habilitar e desabilitar o botão reiniciar.
 
 function alterarStatusBotao() {
     let botao = document.getElementById('btn-reiniciar');
@@ -46,6 +53,8 @@ function alterarStatusBotao() {
             botao.classList.add('container__botao-desabilitado');
     }
 }
+
+// Limpar informações para reiniciar.
 
 function reiniciar(){
     document.getElementById('quantidade').value = '';
